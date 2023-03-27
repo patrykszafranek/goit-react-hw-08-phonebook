d5d58d415712dfebb8637e9145a3b80dc7ab9497
Patryk Szafranek <patryk.szafranek94@gmail.com>
2023-03-27 21:08:29 +0200

    update app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAuth } from 'hooks';
 import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
 import { refreshUser } from 'redux/auth/operations';
